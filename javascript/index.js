f9cf724ad3d244e4e95dfcd70e7d880f62f4d3ea
alamincoders <developeralamin03@gmail.com>
2023-03-31 08:32:22 +0600

    async function

--- javascript/index.js
+++ javascript/index.js
@@ -1,1 +1,12 @@
 console.log("hello world");
+
+const add = async (num1, num2) => {
+  try {
+    const result = num1 + num2;
+    return result;
+  } catch (err) {
+    console.log(err);
+  }
+};
+
+add(5, 6); 
